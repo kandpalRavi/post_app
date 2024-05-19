e7aceab3bb8ac084fb67b4fb2472a903f5500963
kanpalRavi <kanpalravindra21@gmail.com>
2024-05-19 16:44:13 +0530

    posts app and todo app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import {useState} from "react";
+import './App.css';
+import Posts from "./components/Post/Posts";
+
+function App() {
+ 
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Posts/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post/Posts.jsx
@@ -0,0 +1,9 @@
+function Posts(){
+    return(
+       <div>
+        <h1>this is posts</h1>
+       </div> 
+    )
+}
+
+export default Posts;
