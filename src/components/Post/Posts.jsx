68a03ecee9c5af1d7d48f2915ee7054c3bf5d794
kanpalRavi <kanpalravindra21@gmail.com>
2024-05-20 10:14:40 +0530

    frist app in react using api

--- src/components/Post/Posts.jsx
+++ src/components/Post/Posts.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+function getData(){
+    return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`).then(res =>{
+        return res.json()
+    })
+}
+
 function Posts(){
+    const [posts,setPosts] = useState([]);
+
+    const handleFetchData = async ()=>{
+        try{
+            const data = await getData();
+            setPosts(data);
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    console.log(posts)
     return(
        <div>
-        <h1>this is posts</h1>
+        <h1>Posts</h1>
+        <button onClick={handleFetchData}>GetPosts</button>
+        <ul>
+            {posts.map(item =><li key={item.id}>{item.title}</li>)}
+        </ul>
        </div> 
     )
 }
